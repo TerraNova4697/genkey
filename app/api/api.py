a23bd13b492ebd304cc3efed68c6649045b0b776
TerraNova4697 <nikitabechthold@gmail.com>
2024-06-03 15:35:05 +0500

    Reformatted key license

--- app/api/api.py
+++ app/api/api.py
@@ -1,6 +1,7 @@
 from http.client import HTTPException
 import uuid
 import secrets
+import base64
 
 from services.validation.exceptions import ItemNotFound, ValidationException
 from flask import Blueprint, request
@@ -12,47 +13,45 @@ from database import db
 
 
 api_bp = Blueprint(
-    'api_bp',
+    "api_bp",
     __name__,
-    template_folder='templates',
-    static_folder='static',
-    static_url_path='/assets'
+    template_folder="templates",
+    static_folder="static",
+    static_url_path="/assets",
 )
 
 
-@api_bp.route('/', methods=['POST'])
+@api_bp.route("/", methods=["POST"])
 def genkey():
 
-    if request.method == 'POST':
+    if request.method == "POST":
         req_data = request.get_json()
         keygen = KeyGenerator(req_data)
         try:
             if keygen.is_valid():
                 key = keygen.generate_key()
                 payload = handle_keygen_data(req_data, key)
-                payload['status'] = 200
+                payload["status"] = 200
 
                 return payload
         except ValidationException as exception:
-            message = str(exception).replace('\n', ' ')
+            message = str(exception).replace("\n", " ")
             return {"status": 400, "message": str(message)}
         except ItemNotFound as exception:
-            return {
-                'status': 404,
-                'message': str(exception)
-            }
+            return {"status": 404, "message": str(exception)}
 
 
-@api_bp.route('/generate-id', methods=['GET'])
+@api_bp.route("/generate-id", methods=["GET"])
 def gen_id():
     time_based_uuid = uuid.uuid1()
     secret_key = secrets.token_urlsafe(32)
-    device = Device(device_id=time_based_uuid, secret_key=secret_key)
+    bytes_key = secret_key.encode("ascii")
+
+    base64_bytes = base64.b64encode(bytes_key)
+    base64_key = base64_bytes.decode("ascii")
+    print(secret_key, base64_key)
+    device = Device(device_id=time_based_uuid, secret_key=base64_key)
     db.session.add(device)
     db.session.commit()
 
-    return {
-        'status': 200,
-        'device_id': time_based_uuid,
-        'secret_key': secret_key
-    }
+    return {"status": 200, "device_id": time_based_uuid, "secret_key": base64_key}
--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,33 +1,25 @@
 from datetime import date, datetime
 
-from models import (
-    Company, Person, Key, Payment, person_schema
-)
+from models import Company, Person, Key, Payment, person_schema
 from database import db
 
 
-payment_statuses = {
-    1: 'Pending',
-    2: 'Completed',
-    3: 'Failed'
-}
+payment_statuses = {1: "Pending", 2: "Completed", 3: "Failed"}
 
 
 def handle_keygen_data(data, key):
     company = Company(
-        fullname=data['company'],
-        email=data['email'],
-        phone=data['phone']
+        fullname=data["company"], email=data["email"], phone=data["phone"]
     )
     db.session.add(company)
     db.session.commit()
 
     person = Person(
-        fname=data['person']['fname'],
-        lname=data['person']['lname'],
-        email=data['person']['email'],
-        phone=data['person']['phone'],
-        company=company
+        fname=data["person"]["fname"],
+        lname=data["person"]["lname"],
+        email=data["person"]["email"],
+        phone=data["person"]["phone"],
+        company=company,
     )
     db.session.add(person)
     db.session.commit()
@@ -40,22 +32,27 @@ def handle_keygen_data(data, key):
     db.session.add(key)
     db.session.commit()
 
-    dt = date.fromtimestamp(data['payment']['created_at'])
+    dt = date.fromtimestamp(data["payment"]["created_at"])
     payment = Payment(
-        payment_amount=data['payment']['amount'],
-        status=payment_statuses[data['payment']['status']],
+        payment_amount=data["payment"]["amount"],
+        status=payment_statuses[data["payment"]["status"]],
         created_at=dt,
-        key=key
+        key=key,
     )
     db.session.add(payment)
     db.session.commit()
 
     return {
-        'key': key.key,
-        'issued_for': person_schema.dump(person),
-        'created_at': int(datetime.strptime(key.created_at.strftime("%a, %d %b %Y %H:%M:%S GMT"), "%a, %d %b %Y %H:%M:%S GMT").timestamp()),
-        'company': company.fullname,
-        'phone': company.phone,
-        'email': company.email,
-        'payment_status': payment.status
+        "jwt_token": key.key,
+        "issued_for": person_schema.dump(person),
+        "created_at": int(
+            datetime.strptime(
+                key.created_at.strftime("%a, %d %b %Y %H:%M:%S GMT"),
+                "%a, %d %b %Y %H:%M:%S GMT",
+            ).timestamp()
+        ),
+        "company": company.fullname,
+        "phone": company.phone,
+        "email": company.email,
+        "payment_status": payment.status,
     }
